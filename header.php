d257b2e80f3992bde5d5731aad3b3b8a03e8f83e
RafaelChavarria <thief_raf@hotmail.com>
2020-04-01 11:22:11 -0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+
+  <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  
+  <title>Mis Películas - Colección</title>
+
+
+  <!-- LIBRERÍA BOOTSTRAP -->
+
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+  <!-- FUENTES TIPOGRÁFICAS -->
+
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+  <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+
+  <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+
+
+  <!-- HOJA DE ESTILOS -->
+
+  <link href="css/agency.min.css" rel="stylesheet">
+
+
+</head>
+
+
+
+<body id="page-top">
+
+
+
+  <!-- NAVEGACIÓN -->
+
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+
+    <div class="container">
+
+      <a class="navbar-brand js-scroll-trigger" href="index.php">MisPelículas.com</a>
+
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+
+        Menu
+
+        <i class="fas fa-bars"></i>
+
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+
+        <ul class="navbar-nav text-uppercase ml-auto">
+
+
+
+          <li class="nav-item">
+
+            <a class="nav-link js-scroll-trigger" href="#portfolio">Novedades</a>
+
+          </li>
+
+
+
+          <!-- GÉNEROS -->
+
+          <li class="nav-item dropdown">
+
+	        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+	          Géneros
+
+	        </a>
+
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+	          <a class="dropdown-item" href="#">Acción</a>
+
+	          <a class="dropdown-item" href="#">Aventura</a>
+
+	          <a class="dropdown-item" href="#">Comedia</a>
+
+	          <a class="dropdown-item" href="#">Drama</a>
+
+	          <a class="dropdown-item" href="#">Terror</a>
+
+	          <div class="dropdown-divider"></div>
+
+	          <a class="dropdown-item" href="#">Todos</a>
+
+	        </div>
+
+	      </li> <!-- TERMINA GÉNEROS --> 
+
+
+
+	      <li class="nav-item">
+
+            <a class="nav-link js-scroll-trigger" href="#">Directores</a>
+
+          </li>
+
+
+
+          <li class="nav-item">
+
+            <a class="nav-link js-scroll-trigger" href="#team">Equipo</a>
+
+          </li>
+
+          <li class="nav-item">
+
+            <a class="nav-link js-scroll-trigger" href="#contact">Contacto</a>
+
+          </li>
+
+        </ul>
+
+      </div>
+
+    </div>
+
+  </nav>
